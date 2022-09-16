a6e61b8c3f69a4568629943ee804dd01d47e6308
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-09-16 21:52:25 +0530

    added play pause button with functionality

--- src/Pomodoro.js
+++ src/Pomodoro.js
@@ -1,37 +1,46 @@
 import React, { useEffect, useState } from 'react'
 import PauseOutlinedIcon from '@mui/icons-material/PauseOutlined';
+import PlayArrowRoundedIcon from '@mui/icons-material/PlayArrowRounded';
 
 function Pomodoro() {
     const [minute, setMinute] = useState(25)
     const [second, setSecond] = useState(0)
     const [message, setMessage] = useState(false)
+    const [paused, setPaused] = useState(false)
 
     useEffect(() => {
         let intarval = setInterval(() => {
             clearInterval(intarval);
+            if (!paused) {
 
-            if (second === 0) {
-                if (minute !== 0) {
-                    setMinute(minute - 1)
-                    setSecond(59)
+                if (second === 0) {
+                    if (minute !== 0) {
+                        setMinute(minute - 1)
+                        setSecond(59)
+                    } else {
+                        const breakMinutes = message ? 24 : 4;
+                        const breakSeconds = 59;
+
+                        setMinute(breakMinutes)
+                        setSecond(breakSeconds)
+                        setMessage(!message)
+                    }
                 } else {
-                    const breakMinutes = message ? 24 : 4;
-                    const breakSeconds = 59;
-
-                    setMinute(breakMinutes)
-                    setSecond(breakSeconds)
-                    setMessage(!message)
+                    setSecond(second - 1)
                 }
             } else {
-                setSecond(second - 1)
+                return;
             }
-        }, 10);
-    }, [minute, second, message])
+        }, 1000);
+        return () => clearInterval(intarval)
+    }, [minute, second, message, paused])
 
     const restartHandler = () => {
         setMinute(25)
         setSecond(0)
     }
+
+
 
     return (
         <div className='pomodoro'>
@@ -40,9 +49,17 @@ function Pomodoro() {
             </div>
 
             <div className='timer'>{minute < 10 ? "0" + minute : minute}:{second < 10 ? "0" + second : second}</div>
-            <button className='restart-btn' onClick={restartHandler}>Restart</button>
-            <PauseOutlinedIcon />
+            <button className='restart-btn' onClick={restartHandler}>Reset</button>
 
+            <div className='icons'>
+                {
+                    paused ?
+                        <div onClick={() => setPaused(false)}> <PlayArrowRoundedIcon fontSize="large" /></div>
+                        :
+                        <div onClick={() => setPaused(true)}> <PauseOutlinedIcon fontSize="large" /></div>
+                }
+
+            </div>
         </div>
     )
 }
